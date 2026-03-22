a71d097296574aaa2bbbfe66fd43225de58427e5
ThePiklOfTime <nicotolvanen@gmail.com>
2026-03-22 22:20:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/Viikko10/CarData.java
@@ -0,0 +1,23 @@
+package com.example.Viikko10;
+
+public class CarData {
+    private String type;
+    private int amount;
+
+    public CarData(String type, int amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/Viikko10/CarDataStorage.java
@@ -0,0 +1,44 @@
+package com.example.Viikko10;
+
+import java.util.ArrayList;
+
+public class CarDataStorage {
+    private String city;
+    private int year;
+    ArrayList<CarData> carData = new ArrayList<>();
+
+    static private CarDataStorage help;
+
+    private CarDataStorage() {
+    }
+
+    static public CarDataStorage getInstance(){
+        if (CarDataStorage.help == null) {
+            help = new CarDataStorage();
+        }
+        return help;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public ArrayList<CarData> getCarData() {
+        return carData;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void addCarData(CarData carData) {
+
+    }
+}
